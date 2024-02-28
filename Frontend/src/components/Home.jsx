22b3f2c66f51177056b71da59c51dfad952f2f0a
taylyan <tat_milk@abv.bg>
2024-02-28 16:09:22 +0200

    20

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect } from 'react'
-import '../css/Home.css'
+import BannerImage from '../assets/security.jpg'
+import '../styles/Home.css';
 import axios from 'axios'
 
 const Home = () => {
   
   return (
-    <div className='hero'>
-        <div className="hero-content">
-            <h1 className='hero-text'>Book Shop</h1>
-            <p className='hero-description'>
-            Browse the collection of our best top interresting Books.
-            you will definitely find what you are looking for.
-            </p>
+    <div className="home" style={{ backgroundImage: `url(${BannerImage})` }}>
+        <div className="headerContainer" >
+
+            <h1> Securiti is nymber one priority</h1>
+            <p>For every valuble place</p>
+            
+                    
         </div>
-        <div className="hero-image"></div>
     </div>
   )
 }
