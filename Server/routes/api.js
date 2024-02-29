1b4e8382b30936c44906678afd57f86baf29d67d
taylyan <tat_milk@abv.bg>
2024-02-29 14:52:32 +0200

    thinkspeak:3

--- Server/routes/api.js
+++ Server/routes/api.js
@@ -1,9 +1,6 @@
 import express from 'express'
 import axios from "axios"
 
-const express = require('express');
-const axios = require('axios');
-
 const router = express.Router();
 
 const thingSpeakAPI = 'https://api.thingspeak.com/channels/2295351/fields/1/last.json?api_key=7JNHTJGWR1NBR2OX';
