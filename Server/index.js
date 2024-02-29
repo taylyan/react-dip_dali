c665c6c6b3722e01c885f3e4a7606016f740f12a
taylyan <tat_milk@abv.bg>
2024-03-01 00:22:45 +0200

    thinkspeak:23

--- Server/index.js
+++ Server/index.js
@@ -36,19 +36,7 @@ app.get('/dashboard', async (req, res) => {
         return res.json(err)
     }
 })
-app.useEffect(() => {
-    const fetchData = async () => {
-        try {
-            const response = await axios.get('/api/thingspeak');
-            console.log(response.data.field1);
-            setFieldValue(response.data.field1);
-        } catch (error) {
-            console.error('Error fetching data from ThingSpeak:', error);
-        }
-    };
 
-    fetchData();
-}, []);
 
 app.listen(process.env.PORT, () => {
     console.log("Server is Running");
