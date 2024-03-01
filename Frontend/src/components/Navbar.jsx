6e5474042903f4ee0758f288c6472178ce887691
taylyan <tat_milk@abv.bg>
2024-03-01 15:00:46 +0200

    frontbutifull:2

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
 import { Link } from 'react-router-dom'
 import Logo from '../assets/logo_gerb.png'
 import '../css/Navbar.css'
