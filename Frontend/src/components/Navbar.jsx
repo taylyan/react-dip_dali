65c10baab4e4a45bbf00ab0971091ce371e1d4c3
taylyan <tat_milk@abv.bg>
2024-03-03 14:02:12 +0200

    to user 1.2

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = ({ role }) => {
           <Link to="/books" className='navbar-link'>Books</Link>
         {role === "admin" && <>
           <Link to="/addbook" className="navbar-link">Add Book</Link>
-          <Link to="/adduser" className="navbar-link">Add Клиент</Link>
+          <Link to="/adduser" className="navbar-link">Add Student</Link>
           <Link to="/dashboard" className="navbar-link">Dashboard</Link>
           <Link to="/thingspeak" className="navbar-link">Thingspeak</Link>
 
@@ -35,7 +35,7 @@ const Navbar = ({ role }) => {
         <Link to="/books" className='navbar-link'>Books</Link>
         {role === "admin" && <>
           <Link to="/addbook" className="navbar-link">Add Book</Link>
-          <Link to="/addstudent" className="navbar-link">Add Student</Link>
+          <Link to="/adduser" className="navbar-link">Add Student</Link>
           <Link to="/dashboard" className="navbar-link">Dashboard</Link>
           <Link to="/thingspeak" className="navbar-link">Thingspeak</Link>
 
