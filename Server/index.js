011d01bc040df63d9e43f6c63081a5fbef77d39e
taylyan <tat_milk@abv.bg>
2024-03-07 16:58:24 +0200

    email code 0.5

--- Server/index.js
+++ Server/index.js
@@ -11,8 +11,8 @@ import { Device } from './models/Device.js'
 import { User } from './models/User.js'
 import { Admin } from './models/Admin.js'
 import { apiRouter } from './routes/thingspeakApi.js'
+import { cronJobs } from './cronjobs/emailScheduler.js'
 
-const cronJobs = require('./cronjobs/emailScheduler.js');
 const app = express()
 app.use(express.json())
 app.use(cors({
