75aa31b38653113973b32016bdd63a142123afbb
taylyan <tat_milk@abv.bg>
2024-03-08 13:31:14 +0200

    email code 1.1

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,6 @@ import { Device } from './models/Device.js'
 import { User } from './models/User.js'
 import { Admin } from './models/Admin.js'
 import { apiRouter } from './routes/thingspeakApi.js'
-import { scheduleEmails } from './cronjobs/emailScheduler.js'
 
 const app = express()
 app.use(express.json())
